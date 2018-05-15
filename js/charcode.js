41299a211cafaa9744fead901cd565f666273939
Unknown <szqq602@163.com>
2018-05-15 15:25:52 +0800

    字符串转 ASCII 码再转 16 进制字符串

---
+++ js/charcode.js
@@ -0,0 +1,26 @@
+
+// 十进制的 int 转 十六进制 的 string
+function int10ToStr16(i10) {
+  var s16 = i10.toString(16)
+  // console.log('s16', s16);
+  if (s16.length == 1) {
+    s16 = '0' + s16
+  }
+  return s16
+}
+
+// 把一个字符串
+// 变成 16进制 字符串
+function stringToString16(s) {
+  // var arr1 = []
+  // for (var i = 0; i < input.length; i++) {
+  //   arr1.push(input.charCodeAt(i))
+  // }
+
+  var arr1 = Array.prototype.map.call(s, s1 => s1.charCodeAt(0))
+
+  console.log('arr1', arr1);
+
+  var arr2 = arr1.map(int10ToStr16)
+  return arr2.join('')
+}
